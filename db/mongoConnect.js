4009f580c3b762240f3b80c2967870ff54ebee97
Shaked Yosef Sofer <54442082+ShakedYosefSofer@users.noreply.github.com>
2024-03-06 20:57:55 +0200

    Add files via upload

---
+++ db/mongoConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+main().catch(err => console.log(err));
+
+async function main() {
+  // יש לשנות בסוף הכתובת לשם המסד נתונים שאנחנו עובדים מולו בקלאסטר
+  // await mongoose.connect('mongodb://127.0.0.1:27017/ariel23_ev');
+  // console.log("connect mongo ariel23_ev local");
+  await mongoose.connect(process.env.MONGO_CONNECT);
+  console.log("connect mongo ariel23_ev cloud");
+  console.log("connect mongo ariel23_ev local");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
